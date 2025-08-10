0fccc5732284ea28779609bd4147696a3ac65211
Seckin038 <seckin52@hotmail.com>
2025-08-10 12:29:38 +0200

    refactor: improve roles guard

--- backend/src/common/guards/roles.guard.ts
+++ backend/src/common/guards/roles.guard.ts
@@ -13,19 +13,23 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const roles =
-      this.reflector.get<Role[]>('roles', context.getHandler()) ||
-      this.reflector.get<Role[]>('roles', context.getClass());
+    const roles = this.reflector.getAllAndOverride<Role[]>('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
     if (!roles || roles.length === 0) {
       return true;
     }
-    const req = context
+
+    const { user } = context
       .switchToHttp()
       .getRequest<Request & { user?: { role: Role } }>();
-    const user = req.user;
+
     if (user && roles.includes(user.role)) {
       return true;
     }
+
     throw new ForbiddenException();
   }
 }
