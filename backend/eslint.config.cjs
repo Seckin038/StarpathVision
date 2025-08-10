4343584eb2c6c32590b1a38f43ddeeda4133a232
Seckin038 <seckin52@hotmail.com>
2025-08-10 09:29:26 +0200

    chore: restore migrate script

---
+++ backend/eslint.config.cjs
@@ -0,0 +1,19 @@
+const js = require('@eslint/js');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+];
