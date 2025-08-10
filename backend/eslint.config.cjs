7421857da75f3cb5cef8fd53764be8ba95db43a3
Seckin038 <seckin52@hotmail.com>
2025-08-10 11:28:03 +0200

    Update eslint.config.cjs

--- backend/eslint.config.cjs
+++ backend/eslint.config.cjs
@@ -1,33 +1,21 @@
 const globals = require('globals');
-const tsPlugin = require('@typescript-eslint/eslint-plugin');
-const tsParser = require('@typescript-eslint/parser');
+const tseslint = require('typescript-eslint');
 const jest = require('jest');
 const js = require('@eslint/js');
 
 module.exports = [
   js.configs.recommended,
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-      parser: tsParser,
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-    },
-    rules: {
-      ...tsPlugin.configs.recommended.rules,
-    },
-  },
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.spec.ts'],
     languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
+      globals: { ...globals.node, ...globals.jest },
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      globals: { ...globals.node },
     },
   },
 ];
