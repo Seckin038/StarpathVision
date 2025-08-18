0811f009538bb5760fb7eeb5eb2af29bbbb8673e
Seckin038 <seckin52@hotmail.com>
2025-08-18 23:02:30 +0200

    chore: add sitemap generation

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const About = () => (
+  <div className="p-4">
+    <h1 className="text-2xl font-bold">About</h1>
+    <p>Learn more about our project.</p>
+  </div>
+);
+
+export default About;
